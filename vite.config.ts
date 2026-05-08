c2bc83580f5b7e9783b74b6f67679e81d55b9cf8
gweecl <44865263+gweecl@users.noreply.github.com>
2026-05-08 16:39:49 +0800

    Set base path for Vite configuration

--- vite.config.ts
+++ vite.config.ts
@@ -3,6 +3,7 @@ import tailwindcss from "@tailwindcss/vite";
 import { defineConfig } from "vite";
 
 export default defineConfig({
+  base: "/klse-stock-quotes/",
   plugins: [tailwindcss(), reactRouter()],
   resolve: {
     tsconfigPaths: true,
